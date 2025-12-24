d902e71c922f022b2601b835587598fd0c39b85d
rajofearth <rajofearth@proton.me>
2025-12-24 18:57:54 +0530

    Add initial component structure and dependencies
    - Created `components.json` for component configuration.
    - Added multiple UI components including buttons, dialogs, and forms.
    - Integrated new dependencies such as `@base-ui/react` and `@radix-ui` for UI elements.
    - Updated `globals.css` to include new styles and animations.
    - Refactored layout and main page to utilize new components.
    - Introduced utility functions for class name management.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Public_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const fontSans = Public_Sans({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,9 +15,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={fontSans.variable}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className="antialiased dark"
       >
         {children}
       </body>
